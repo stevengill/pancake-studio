abcab9936e776bb18c673bc0d9fffdcf57002ae3
Steve Gill <stevengill@slack-corp.com>
2020-10-28 11:20:25 -0700

    added convert button logic

--- public/index.js
+++ public/index.js
@@ -1,16 +1,31 @@
 function convertURL() {
+    const urlBox = document.getElementById('urlbox');
+    let input = urlBox.value;
+    if (input === '') {
+        input = urlBox.placeholder
+    }
+
+    // check for valid slack url input
+    const regex = /hooks.slack.com\/workflows/g;
+    const valid = input.match(regex)
+    console.log(valid)
+
+    if (!valid) {
+        console.error('URL provided was not a valid hook.slack.com/workflows url')
+        return
+    }
+
     document.querySelector('#url-box-flip').classList.toggle('flip')
     // document.querySelector('#urlbox').classList.toggle('hide')
     // document.querySelector('#urlbox-pancake-url').classList.toggle('hide')
 
-    const input = document.getElementById('urlbox').value;
-    if (input === '') {
-        console.log('no input')
-    }
+    const url = new URL(input)
+    // change URL hostname to servers host
+    url.hostname = window.location.host
 
-    // check for valid slack url input
+    // update second input box with new url
+    document.getElementById('urlbox-pancake-url').value = url.href
+    
 
-    // convert URL
-    console.log(input)
-    console.log(window.location.href)
+
 }
