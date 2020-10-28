c83feb8ea319fdad3ffd60bde92fda3165acbdf9
Michael Brooks <mbrooks@slack-corp.com>
2020-10-28 10:34:11 -0700

    Toggle Slack URL and Pancake URL

--- public/index.js
+++ public/index.js
@@ -1,4 +1,7 @@
 function convertURL() {
+    document.querySelector('#urlbox').classList.toggle('hide')
+    document.querySelector('#urlbox-pancake-url').classList.toggle('hide')
+
     const input = document.getElementById('urlbox').value;
     if (input === '') {
         console.log('no input')
